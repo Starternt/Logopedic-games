b2b20ca019af2d29c45ddd2c231fe83db61ba78b
Administrator <Starternh@gmail.com>
2018-03-20 20:08:00 +0300

    added category My awards Changed style.css due of new category

--- resources/views/categories/_categories.blade.php
+++ resources/views/categories/_categories.blade.php
@@ -26,6 +26,9 @@
                 <a href="/information" id="category5"><i class="fa fa-fw fa-info"></i> Информация для родителей</a>
             </li>
             <li>
+                <a href="/information" id="category7"><i class="fa fa-fw fa-trophy"></i> Мои достижения</a>
+            </li>
+            <li>
                 <a href="/about" id="category6"><i class="fa fa-fw fa-phone"></i> Контакты</a>
             </li>
         </ul>
